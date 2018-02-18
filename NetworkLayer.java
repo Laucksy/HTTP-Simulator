77c000888520e2f5acc068a721a366d8ba0c2a72
Erik Laucks <phisigmapi314@gmail.com>
2018-02-17 22:11:30 -0500

    Added propagation and transmission delays to network layer

--- NetworkLayer.java
+++ NetworkLayer.java
@@ -1,4 +1,8 @@
+import java.util.concurrent.TimeUnit;
+
 public class NetworkLayer {
+  private final float PROPAGATION_DELAY = 0.25;
+  private final float TRANSMISSION_DELAY_RATE = 0.05;
   private LinkLayer linkLayer;
 
   public NetworkLayer(boolean server, int addr) {
@@ -6,11 +10,13 @@ public class NetworkLayer {
   }
 
   public void send(byte[] payload) {
+    TimeUnit.SECONDS.sleep(TRANSMISSION_DELAY_RATE * payload.length);
     linkLayer.send(payload);
   }
 
   public byte[] receive() {
     byte[] payload = linkLayer.receive();
+    TimeUnit.SECONDS.sleep(PROPAGATION_DELAY);
     return payload;
   }
 }

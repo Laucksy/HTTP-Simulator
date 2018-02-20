60bd8d597cea8278eb8f2f70bb785c159299fe87
Erik Laucks <phisigmapi314@gmail.com>
2018-02-20 14:26:18 -0500

    Modified runners to use HTTP and Connection

--- ProxyApp.java
+++ ProxyApp.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 //This class represents the client application
 public class ProxyApp {
@@ -7,20 +8,24 @@ public class ProxyApp {
     //create a new transport layer for proxy server (hence true), on the correct port
     TransportLayer transportLayer = new TransportLayer(true, TransportLayer.PROXY_LISTENING_PORT);
     //open a client connection to the server (hence false), on the correct port
-    TransportLayer transportLayer2 = new TransportLayer(false, TransportLayer.WEB_LISTENING_PORT);
+    // TransportLayer transportLayer2 = new TransportLayer(false, TransportLayer.WEB_LISTENING_PORT);
 
     while(true) {
+      System.out.println("while loop");
       //get line from client
       byte[] byteArray = transportLayer.receive();
-      if(byteArray == null)
-          break;
+      if(byteArray == null) {
+        TimeUnit.MILLISECONDS.sleep(250);
+        continue;
+      }
 	    System.out.println("From Client: " + byteArray);
       //send line to server
-      transportLayer2.send(byteArray);
-      //recieve line from server and forward to client
-      byteArray = transportLayer2.receive();
-      System.out.println("From Server: " + byteArray);
-      transportLayer.send(byteArray);
+      HTTP http = new HTTP();
+      String response = http.get("1.0", "test", 8888);
+      System.out.println(response);
+
+      System.out.println("From Server: " + response);
+      transportLayer.send(response.getBytes());
     }
   }
 }
--- ServerApp.java
+++ ServerApp.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.concurrent.TimeUnit;
 
 //This class represents the server application
 public class ServerApp {
@@ -7,12 +8,15 @@ public class ServerApp {
     //create a new transport layer for server (hence true) with server adddress
     TransportLayer transportLayer = new TransportLayer(true, TransportLayer.WEB_LISTENING_PORT);
     while(true) {
+      System.out.println("While loop");
       //receive message from client, and send the "received" message back.
       byte[] byteArray = transportLayer.receive();
 
       //if client disconnected
-      if(byteArray == null)
-        break;
+      if(byteArray == null) {
+        TimeUnit.MILLISECONDS.sleep(250);
+        continue;
+      }
       String str = new String ( byteArray );
       System.out.println(str);
       String line = "received";

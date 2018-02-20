e5c67219c38ad91ded13c2da89b3891a62bbb88d
Zura Mestiashvili <prosperrriii@gmail.com>
2018-02-20 13:13:54 -0500

    Working on parser

--- CLMLParser.java
+++ CLMLParser.java
@@ -23,8 +23,18 @@ public class CLMLParser
      */
     public CLMLParser(String doc)
     {
-         Pattern resourcePattern = Pattern.compile("\\*\\*\\*[a-zA-Z]+\\s[a-zA-Z]+\\s[a-zA-Z]*\\s\\*\\*\\*");
+         Pattern resourcePattern = Pattern.compile("\\*{3} +(.+) +(.+) +(.+ +)?\\*{3}");
          m = resourcePattern.matcher(doc);
-         System.out.println(m);
+         
+         while (m.find()) {
+             System.out.println("\u001B[33m ===================== \u001B[0m\n");
+             System.out.println("\u001B[31m Type: " + m.group(1) + "\u001B[0m");
+             System.out.println("\u001B[34m Source: " + m.group(2) + "\u001B[0m");
+             if(m.group(3) != null)
+                System.out.println("\u001B[32m Title: " + m.group(3) + "\u001B[0m");
+
+         }
+         
+         System.out.println("\n\u001B[32m---------- Parsing is done ----------\u001B[0m");
     }
 }

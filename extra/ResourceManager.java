f0a7e7b64ece9f4e55df4325ba295494a0ca2f1d
Wassim Gharbi <wassgha@gmail.com>
2018-02-23 18:13:20 -0500

    More commenting

--- extra/ResourceManager.java
+++ extra/ResourceManager.java
@@ -43,13 +43,22 @@ public class ResourceManager
 
     public Resource getCachedResource(String url, int port) {
         long start = System.currentTimeMillis();
+
+        // Check if the requested resource exists in the cache
+
         if (this.resources.containsKey(url) /* && localCachingEnabled */) {
+
+          // If it does exist, then we request the resource from the server to
+          // see if it was modified
+
           if (ClientApp.DEBUG_MODE)
             System.out.println("Found locally cached resource " + url);
           Resource result = this.resources.get(url);
 
           HTTPEngine.HTTPResponse res =  http.get("1.1", url, port, result.loadedDate);
 
+          // If the server return 304, then the file already exists in the cache
+          // so we don't overwrite it
           if (res.status != 304) {
             result.loadedDate = System.currentTimeMillis();
             result.file = res.data;
@@ -57,18 +66,26 @@ public class ResourceManager
 
           result.response = res;
 
+          // Print out how long it took to process the request
           long end = System.currentTimeMillis();
           if (ClientApp.DEBUG_MODE)
             System.out.println("Took : " + (end - start) + " millis");
 
           return result;
         } else {
+
+          // If it doesn't then we request it from the server for the first time
+
           if (ClientApp.DEBUG_MODE)
             System.out.println("Did not find locally cached resource " + url);
+
           Resource result = this.loadUrl(url, port);
+
+          // Print out how long it took to process the request
           long end = System.currentTimeMillis();
           if (ClientApp.DEBUG_MODE)
             System.out.println("Took : " + (end - start) + " millis");
+
           return result;
         }
     }
